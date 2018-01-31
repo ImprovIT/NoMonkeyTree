3345fa307a918e1fad80ba1b5d32f81cde916a6d
Alexandre <alexandre.lim@hotmail.com>
2018-01-31 14:15:56 +0100

    Init project

---
+++ ITI.GameOfLife.Tests/T1GameOfLifeTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using NUnit.Framework;
+using FluentAssertions;
+using System.Linq.Expressions;
+using System.Xml;
+
+namespace ITI.GameOfLife.Tests
+{
+
+    class MyExpressionVisitor : ExpressionVisitor
+    {
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            Console.WriteLine("Visit Binary");
+            return node;
+        }
+    }
+
+
+    [TestFixture]
+    public class T1GameOfLifeTests
+    {
+        [Test]
+        public void init_ast()
+        {
+            var a = Expression.Constant(2);
+            var b = Expression.Constant(3);
+            var c = Expression.Constant(1);
+
+            var sumExpr = Expression.Add(a, b);
+            var sumExpr2 = Expression.Add(sumExpr, c);
+
+            var visitor = new MyExpressionVisitor();
+            //visitor.VisitBinary
+
+            Console.WriteLine(visitor.Visit(sumExpr2));
+
+
+            //Console.WriteLine(sumExpr.ToString());
+            var sumResult = Expression.Lambda<Func<int>>(sumExpr).Compile()();
+
+            //Console.WriteLine(sumResult);
+        }
+
+        [Test]
+        public void ast_addition()
+        {
+            /*Game game = new Game();
+            int a = new Random().Next(Int32.MinValue, Int32.MaxValue);
+            int b = new Random().Next(Int32.MinValue, Int32.MaxValue);
+
+            Expression expression = game.AstAddition(a, b);
+            var sumResult = Expression.Lambda<Func<int>>(expression).Compile()();
+
+            sumResult.Should().Be(a + b);
+        */
+        }
+
+        [Test]
+        public void ast_rpg_story()
+        {
+            /*   Game game = new Game();
+
+               var exp1 = game.Damaged(10, 5);
+             //  var exp2 = game.Healed(9, 2);
+
+               var res1 = Expression.Lambda<Func<int>>(exp1).Compile()();
+           //    var res2 = Expression.Lambda<Func<int>>(exp2).Compile()();
+
+               var visitor = new RpgExpressionVisitor();
+
+               //Console.WriteLine(res1.ToString());
+
+               Console.WriteLine("visitor : " + visitor.Visit(exp1));
+               Console.WriteLine("exp1 : " + exp1);
+               Console.WriteLine("res 1 : " + res1);
+
+               visitor.Visit(exp1).ToString().Should().Be("10 damaged 5");
+               res1.Should().Be(5);
+               */
+        }
+
+        [Test]
+        public void ast_simple_operator_works()
+        {
+            BinaryExpression ast = Game.AstSimpleOperator();
+
+
+            ast.NodeType.Should().Be(ExpressionType.Divide);
+
+            BinaryExpression astMultiply;
+            Expression const1;
+
+            if (ast.Left.NodeType == ExpressionType.Multiply)
+            {
+                astMultiply = (BinaryExpression)ast.Left;
+                const1 = ast.Right;     
+            }
+            else
+            {
+                astMultiply = (BinaryExpression) ast.Right;
+                const1 = ast.Left;
+            }
+
+            astMultiply.NodeType.Should().Be(ExpressionType.Multiply);
+            const1.NodeType.Should().Be(ExpressionType.Constant);
+            const1.ToString().Should().Be("4");
+
+
+        }
+
+    }
+}
---
+++ ITI.GameOfLife/Game.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ITI.GameOfLife
+{
+    public static class Game
+    {
+        /// <summary>
+        /// ( 3 + 5 ) * 3 / 4
+        /// </summary>
+        public static BinaryExpression AstSimpleOperator()
+        {
+            var a = Expression.Add(Expression.Constant(3), Expression.Constant(5));
+            var b = Expression.Multiply(Expression.Constant(3), a);
+            return Expression.Divide(b, Expression.Constant(4));
+        }
+        /*
+        public Expression AstAddition(int a, int b)
+        {
+            return Expression.Add(Expression.Constant(a), Expression.Constant(b));
+        }
+
+        public void CreateCharacter(string Name, int hp)
+        {
+
+        }
+
+        public Expression Damaged(int charHp, int hp)
+        {
+            return Expression.Subtract(Expression.Constant(charHp), Expression.Constant(hp));
+        }
+
+        public Expression Healed(int charHp, int hp)
+        {
+            return Expression.Add(Expression.Constant(charHp), Expression.Constant(hp));
+        }
+
+    }
+    public class RpgExpressionVisitor : ExpressionVisitor
+    {
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            // base hp
+            var leftNode = this.Visit(node.Left);
+
+            // hp modificator
+            switch (node.NodeType)
+            {
+                case ExpressionType.Add:
+                    sb.Append(leftNode);
+                    sb.Append(" healed ");
+                    //Console.Write(" healed ");
+                    break;
+                case ExpressionType.Subtract:
+                    sb.Append(leftNode);
+                    sb.Append(" damaged ");
+                    //Console.Write(leftNode.ToString());
+                    //Console.Write(" damaged ");
+                    break;
+
+            }
+
+            // quantity of hp affected
+            var rightNode = this.Visit(node.Right);
+            sb.Append(rightNode);
+            //Console.Write(rightNode.ToString());
+            //Console.WriteLine(" HP.");
+
+            return Expression.Constant(sb);
+        }
+    */
+    }
+}
