92f9412e3a9bfbf6db117eb5b2f1d635ac93c327
Aymeric <richard.aymeric.92@gmail.com>
2018-02-01 18:07:34 +0100

    Cleaning tests

--- ITI.GameOfLife.Tests/T1GameOfLifeTests.cs
+++ ITI.GameOfLife.Tests/T1GameOfLifeTests.cs
@@ -23,66 +23,6 @@ namespace ITI.GameOfLife.Tests
     public class T1GameOfLifeTests
     {
         [Test]
-        public void init_ast()
-        {
-            var a = Expression.Constant(2);
-            var b = Expression.Constant(3);
-            var c = Expression.Constant(1);
-
-            var sumExpr = Expression.Add(a, b);
-            var sumExpr2 = Expression.Add(sumExpr, c);
-
-            var visitor = new MyExpressionVisitor();
-            //visitor.VisitBinary
-
-            Console.WriteLine(visitor.Visit(sumExpr2));
-
-
-            //Console.WriteLine(sumExpr.ToString());
-            var sumResult = Expression.Lambda<Func<int>>(sumExpr).Compile()();
-
-            //Console.WriteLine(sumResult);
-        }
-
-        [Test]
-        public void ast_addition()
-        {
-            /*Game game = new Game();
-            int a = new Random().Next(Int32.MinValue, Int32.MaxValue);
-            int b = new Random().Next(Int32.MinValue, Int32.MaxValue);
-
-            Expression expression = game.AstAddition(a, b);
-            var sumResult = Expression.Lambda<Func<int>>(expression).Compile()();
-
-            sumResult.Should().Be(a + b);
-        */
-        }
-
-        [Test]
-        public void ast_rpg_story()
-        {
-            /*   Game game = new Game();
-
-               var exp1 = game.Damaged(10, 5);
-             //  var exp2 = game.Healed(9, 2);
-
-               var res1 = Expression.Lambda<Func<int>>(exp1).Compile()();
-           //    var res2 = Expression.Lambda<Func<int>>(exp2).Compile()();
-
-               var visitor = new RpgExpressionVisitor();
-
-               //Console.WriteLine(res1.ToString());
-
-               Console.WriteLine("visitor : " + visitor.Visit(exp1));
-               Console.WriteLine("exp1 : " + exp1);
-               Console.WriteLine("res 1 : " + res1);
-
-               visitor.Visit(exp1).ToString().Should().Be("10 damaged 5");
-               res1.Should().Be(5);
-               */
-        }
-
-        [Test]
         public void ast_simple_operator_should_works()
         {
             BinaryExpression ast = Game.AstSimpleOperator();
